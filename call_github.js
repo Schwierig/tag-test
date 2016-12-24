c6bc9e2160d3244802684b6a8ed500430451e3d4
Amir El Sayed <aes@basecom.de>
2016-12-24 12:47:48 +0100

    no message

--- call_github.js
+++ call_github.js
@@ -2,7 +2,9 @@ chrome.storage.sync.get({'interval': 5000}, function (items) {
     setInterval(function () {
         chrome.storage.sync.get('repositories', function (items) {
             for (var repositoryName in items.repositories) {
-                fetchTags(repositoryName, items.repositories);
+                if(items.repositories.hasOwnProperty(repositoryName)) {
+                    fetchTags(repositoryName, items);
+                }
             }
         });
     }, parseInt(items.interval));
